cc2c4b43ce41c9b8219c686a4a7b6637731adccf
Francisco Coromoto <coromoto.francisco@gmail.com>
2021-09-30 20:59:14 -0300

    [feature/base] ajuste do dockerignore e removendo arquivos entrypoint.sh e healthcheck.sh

--- healthcheck.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-curl --silent --fail --request GET \
-                http://localhost:${SERVER_PORT}/actuator/health \
-                | jq --exit-status '.status == "UP"' || exit 1
